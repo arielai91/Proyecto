7f42fea53708f062a098b4bd528aa8e43c8254ee
XolHed3RP <105742572+arielai91@users.noreply.github.com>
2024-11-10 11:48:01 -0500

    Backend Version 1.4.2 - Se implementaron operaciones CRUD basicas para la base de datos mongoDB y la carga de la imagen desde un bucket AWS

--- Backend/routes/casilleroRoutes.js
+++ Backend/routes/casilleroRoutes.js
@@ -9,7 +9,14 @@ class CasilleroRoutes {
     this.initializeRoutes();
   }
 
+  logRequest(req, res, next) {
+    console.log(`Petición: ${req.method}, Ruta: ${req.originalUrl}`);
+    next();
+  }
+
   initializeRoutes() {
+    // Middleware para imprimir el tipo de petición y la ruta
+    this.router.use(this.logRequest);
     this.router.post(
       "/casilleros",
       [
